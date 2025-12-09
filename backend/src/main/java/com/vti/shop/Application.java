c7ae7dc1a62193d710b7167afd1a2db25a05c23b
lean1835 <lean1835.vac@gmail.com>
2025-12-10 00:30:24 +0700

    aaa

--- backend/src/main/java/com/vti/shop/Application.java
+++ backend/src/main/java/com/vti/shop/Application.java
@@ -3,7 +3,7 @@ package com.vti.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
