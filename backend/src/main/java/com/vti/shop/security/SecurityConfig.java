8b7a3c0d0b0ccb0b042ae94cebcf31034756f794
lean1835 <lean1835.vac@gmail.com>
2025-12-09 22:55:37 +0700

    huhu

--- backend/src/main/java/com/vti/shop/security/SecurityConfig.java
+++ backend/src/main/java/com/vti/shop/security/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+//huhu
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
